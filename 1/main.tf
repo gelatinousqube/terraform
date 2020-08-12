7bacd468d8337d638c9500d1f903643338a61f55
leolambertucci <leonardo.lambertucci@ffptn3618.farfetch.local>
2020-08-12 10:17:51 +0100

    commit

--- 1/main.tf
+++ 1/main.tf
@@ -1,6 +1,13 @@
-provider "azurerm" {
-  version = "=1.35.0"
+terraform {
+    required_providers {
+        azurerm = {
+            source = "hashicorp/azurerm"
+            version = "~>1.35.0"
+        }
+    }
 }
+
+provider "azurerm" {}
 
 # Create a resource group
 resource "azurerm_resource_group" "demo" {
